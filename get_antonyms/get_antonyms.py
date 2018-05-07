bc5645951e0c33a81b844932ca39889b20718a80
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-05-07 01:51:58 -0300

    better variable names

--- get_antonyms/get_antonyms.py
+++ get_antonyms/get_antonyms.py
@@ -4,52 +4,52 @@ import rdflib
 from rdflib import URIRef as URI
 
 MY_PATH = os.path.dirname(os.path.realpath(__file__))
-RDF_FILE = os.path.join(MY_PATH, '../assets/OntoPTv0.6_rdf/OntoPTv0.6.rdfs')
-TXT_FILE = os.path.join(MY_PATH, '../assets/ICMC_USP/triplos.txt')
-OUT_FILE = os.path.join(MY_PATH, '../assets/ants.js/ants.js')
+RDF_FILE = MY_PATH + '/../assets/OntoPTv0.6_rdf/OntoPTv0.6.rdfs'
+TXT_FILE = MY_PATH + '/../assets/ICMC_USP/triplos.txt'
+OUT_FILE = MY_PATH + '/../assets/ants.js/ants.js'
 
-g = rdflib.Graph()
-result = g.parse(RDF_FILE)
+mGraph = rdflib.Graph()
+mGraph.parse(RDF_FILE)
 
-d = {}
+antonymDictionary = {}
 URI_URL = u'http://ontopt.dei.uc.pt/OntoPT.owl#'
-uris = [u'http://ontopt.dei.uc.pt/OntoPT.owl#antonimoAdjDe',
-        u'http://ontopt.dei.uc.pt/OntoPT.owl#antonimoNDe',
-        u'http://ontopt.dei.uc.pt/OntoPT.owl#antonimoVDe']
+uris = [URI_URL + 'antonimoAdjDe',
+        URI_URL + 'antonimoNDe',
+        URI_URL + 'antonimoVDe']
 
-def putInDict(w, a, d):
-  if(w not in d):
-    d[w] = {}
-  d[w][a] = 1
-  if(a not in d):
-    d[a] = {}
-  d[a][w] = 1
+def putInDict(word, antonym, dict):
+  if(word not in dict):
+    dict[word] = {}
+  dict[word][antonym] = 1
+  if(antonym not in dict):
+    dict[antonym] = {}
+  dict[antonym][word] = 1
   return
 
-for u in uris:
-  for ant_pair in g[:URI(u)]:
-      for w in g[ant_pair[0] : URI(URI_URL + 'formaLexical')]:
-        w = w.encode('utf8').decode('utf8')
-        for a in g[ant_pair[1] : URI(URI_URL + 'formaLexical')]:
-          a = a.encode('utf8').decode('utf8')
-          putInDict(w, a, d)
+for uri in uris:
+  for antonymPair in mGraph[:URI(uri)]:
+      for word in mGraph[antonymPair[0] : URI(URI_URL + 'formaLexical')]:
+        word = word.encode('utf8').decode('utf8')
+        for antonym in mGraph[antonymPair[1] : URI(URI_URL + 'formaLexical')]:
+          antonym = antonym.encode('utf8').decode('utf8')
+          putInDict(word, antonym, antonymDictionary)
 
 with open(TXT_FILE, 'r') as infile:
   for line in infile:
     if('ANTONIMO_DE' in line):
       line = re.sub(r'\[.*\] ','', line)
       line_arr = line.split(' ANTONIMO_DE ')
-      w = line_arr[0].decode('utf8').strip()
-      a = line_arr[1].decode('utf8').strip()
-      putInDict(w, a, d)
+      word = line_arr[0].decode('utf8').strip()
+      antonym = line_arr[1].decode('utf8').strip()
+      putInDict(word, antonym, antonymDictionary)
 
 with open(OUT_FILE, 'w') as outfile:
   outfile.write('var ants = {\n')
-  for k in d:
-    s = '  \'' + k.encode('utf8') + '\': ['
-    for a in d[k]:
-      s += '\n    \'' + a.encode('utf8') + '\','
-    s = s[:-1]
-    s += '\n  ],\n'
-    outfile.write(s)
+  for word in antonymDictionary:
+    wordAntonymString = '  \'' + word.encode('utf8') + '\': ['
+    for antonym in antonymDictionary[word]:
+      wordAntonymString += '\n    \'' + antonym.encode('utf8') + '\','
+    wordAntonymString = wordAntonymString[:-1]
+    wordAntonymString += '\n  ],\n'
+    outfile.write(wordAntonymString)
   outfile.write('}\n')
