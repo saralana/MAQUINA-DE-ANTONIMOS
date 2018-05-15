a5f64b582d00a641a5a5e658e2e77ba21b3e44e2
Sara Lana <saralgc@gmail.com>
2018-05-15 11:39:14 -0300

    content script

--- EXTENSION _ traducao antonimica/contentscript.js
+++ EXTENSION _ traducao antonimica/contentscript.js
@@ -1,5 +1,7 @@
 /* http://stackoverflow.com/questions/15604140/replace-multiple-strings-with-multiple-other-strings
 
+https://github.com/thalida/WordBird/blob/master/extension/content_script.js
+
 String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
@@ -22,33 +24,37 @@ var new = replaceAll(str, map);
 //result: "foo, boo, bar"
 */
 
-//START
-var start = document.getElementsByClassName('btn__text listen')[0].getElementsByTagName('p')[0];
-start.click();
-
-// MUTATION OBSERVER de olho no tp.innerHTML e ver se algo mudou
-// se sim, comparar com as variáveis escuta e antonimo
+window.onload = function(){ 
+  console.log("oi");
   
-      //ENCONTRA A TAG do editor de texto que armazena o conteudo stt 
-      var stt = document.getElementsByClassName('ql-editor')[0].getElementsByTagName('p')[0];
+            //START
+            var start = document.getElementsByClassName('btn__text listen')[0].getElementsByTagName('p')[0];
+              console.log(start);
+            start.click();
 
-      //SALVA O CONTEUDO FALADO NA VARIAVEL ESCUTA
-      var escuta = stt.innerHTML;
+            //ENCONTRA A TAG do editor de texto que armazena o conteudo stt 
+            var notepad = document.getElementsByClassName('ql-editor')[0].getElementsByTagName('p')[0];
 
-      //CLEAR
-      stt.innerHTML = '';
-  
-        //Código para tradução antonimica 
-      
-      //FRASE ANTONIMIZADA SALVA NA VARIAVEL ANTONIMO
-      var antonimo = ;
-           
-      //COPIA O ANTONIMO NO EDITOR DE TEXTO
-      tp.innerHTML= antonimo;
+            // mutation observer olhando notepad
+            // se algo mudou, comparar com as variáveis escuta e antonimo
+   
+            //SALVA O CONTEUDO FALADO NA VARIAVEL ESCUTA
+            var escuta = notepad.innerHTML;
+            
+            //CLEAR
+            notepad.innerHTML = '';
 
-      //MANDA COMANDO PARA QUE O ANTONIMO SEJA PRONUNCIADO
-      dictation('tts');
+              //Código para tradução antonimica 
 
-      //CLEAR
-      tp.innerHTML = '';     
+            //FRASE ANTONIMIZADA SALVA NA VARIAVEL ANTONIMO
+            var antonimo = 'outro teste';
+
+            //COPIA O ANTONIMO NO EDITOR DE TEXTO
+            notepad.innerHTML= antonimo;
+
+            //MANDA COMANDO PARA QUE O ANTONIMO SEJA PRONUNCIADO
+            dictation('tts');
+
+            //CLEAR
+            notepad.innerHTML = '';     
 }
